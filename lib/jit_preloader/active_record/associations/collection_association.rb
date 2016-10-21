a6541c26bded8510919d3788cf538a124ac308cd
Kyle d'Oliveira <kyle@goclio.com>
2016-10-21 16:58:27 -0700

    If we didn't preload an association and are globally enabled we make the association jit preloadable

--- lib/jit_preloader/active_record/associations/collection_association.rb
+++ lib/jit_preloader/active_record/associations/collection_association.rb
@@ -9,6 +9,8 @@ class ActiveRecord::Associations::CollectionAssociation
     was_loaded = loaded?
 
     load_target_without_jit.tap do |records|
+      JitPreloader::Preloader.attach(records) if records.any? && !jit_loaded && JitPreloader.globally_enabled?
+
       records.each{ |record| record.jit_n_plus_one_tracking = true } if jit_loaded || !was_loaded
       if !was_loaded && loaded? && owner.persisted? && owner.jit_n_plus_one_tracking
         ActiveSupport::Notifications.publish("n_plus_one_query", 
