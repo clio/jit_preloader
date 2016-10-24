cbdab735ed65038dc3bc637169c757f722726de2
Kyle d'Oliveira <kyle@goclio.com>
2016-10-24 09:52:53 -0700

    Clarify the loaded logic and ensure that we are not triggering N+1 queries on non persisted objects, or on objects that we've already loaded

--- lib/jit_preloader/active_record/associations/collection_association.rb
+++ lib/jit_preloader/active_record/associations/collection_association.rb
@@ -1,20 +1,27 @@
 class ActiveRecord::Associations::CollectionAssociation
 
   def load_target_with_jit
-    if !loaded? && owner.persisted? && owner.jit_preloader
-      owner.jit_preloader.jit_preload(reflection.name)
-      jit_loaded = true
-    end
-
     was_loaded = loaded?
 
-    load_target_without_jit.tap do |records|
-      JitPreloader::Preloader.attach(records) if records.any? && !jit_loaded && JitPreloader.globally_enabled?
+    if !loaded? && owner.persisted? && owner.jit_preloader
+      owner.jit_preloader.jit_preload(reflection.name)
+    end
 
-      records.each{ |record| record.jit_n_plus_one_tracking = true } if jit_loaded || !was_loaded
-      if !was_loaded && loaded? && owner.persisted? && owner.jit_n_plus_one_tracking
-        ActiveSupport::Notifications.publish("n_plus_one_query", 
-                                             source: owner, association: reflection.name)
+    jit_loaded = loaded?
+
+    load_target_without_jit.tap do |records|
+      # We should not act on non-persisted objects, or if it we started this method already loaded
+      if owner.persisted? && !was_loaded
+        # If we went through a JIT preload, then we will have attached another JitPreloader elsewhere
+        JitPreloader::Preloader.attach(records) if records.any? && !jit_loaded && JitPreloader.globally_enabled? 
+
+        # If the records were not pre_loaded
+        records.each{ |record| record.jit_n_plus_one_tracking = true }
+        
+        if !jit_loaded && owner.jit_n_plus_one_tracking
+          ActiveSupport::Notifications.publish("n_plus_one_query", 
+                                               source: owner, association: reflection.name)
+        end
       end
     end
   end
--- lib/jit_preloader/active_record/associations/singular_association.rb
+++ lib/jit_preloader/active_record/associations/singular_association.rb
@@ -1,18 +1,25 @@
 class ActiveRecord::Associations::SingularAssociation
 
   def load_target_with_jit
+    was_loaded = loaded?
+
     if !loaded? && owner.persisted? && owner.jit_preloader
       owner.jit_preloader.jit_preload(reflection.name)
-      jit_loaded = true
     end
 
-    was_loaded = loaded?    
+    jit_loaded = loaded?
 
     load_target_without_jit.tap do |record|
-      record.jit_n_plus_one_tracking = owner.jit_n_plus_one_tracking if record && (jit_loaded || !was_loaded)
-      if !was_loaded && loaded? && owner.persisted? && owner.jit_n_plus_one_tracking
-        ActiveSupport::Notifications.publish("n_plus_one_query", 
-                                             source: owner, association: reflection.name)
+      if owner.persisted? && !was_loaded
+        # If the owner doesn't track N+1 queries, then we don't need to worry about
+        # tracking it on the record. This is because you can do something like:
+        # model.foo.bar (where foo and bar are singular associations) and that isn't always an N+1 query
+        record.jit_n_plus_one_tracking ||= owner.jit_n_plus_one_tracking if record
+
+        if !jit_loaded && owner.jit_n_plus_one_tracking
+          ActiveSupport::Notifications.publish("n_plus_one_query", 
+                                               source: owner, association: reflection.name)
+        end
       end
     end
   end
