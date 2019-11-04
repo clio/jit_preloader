79d6622b83051f37475bfc9395bc31774b141461
Greg Field <greg.field@clio.com>
2019-11-04 15:36:02 -0800

    Breaking up the logic for getting the name used for referencing the table join for clarity

--- lib/jit_preloader/active_record/base.rb
+++ lib/jit_preloader/active_record/base.rb
@@ -42,8 +42,10 @@ module JitPreloadExtension
             association_scope = association_scope.instance_exec(&reflection.scope).reorder(nil) if reflection.scope
 
             # If the query uses an alias for the association, use that instead of the table name
-            table_alias_name = association_scope.references_values.first || aggregate_association.table_name
-            conditions[table_alias_name] = { aggregate_association.foreign_key => primary_ids }
+            table_alias_name = association_scope.references_values.first
+            table_reference = table_alias_name || aggregate_association.table_name
+
+            conditions[table_reference] = { aggregate_association.foreign_key => primary_ids }
 
             # If the association is a STI child model, specify its type in the condition so that it
             # doesn't include results from other child models
@@ -51,13 +53,13 @@ module JitPreloadExtension
             has_type_column = aggregate_association.klass.column_names.include?(aggregate_association.klass.inheritance_column)
             is_child_sti_model = !is_base_class && has_type_column
             if is_child_sti_model
-              conditions[table_alias_name].merge!({ aggregate_association.klass.inheritance_column => aggregate_association.klass.sti_name })
+              conditions[table_reference].merge!({ aggregate_association.klass.inheritance_column => aggregate_association.klass.sti_name })
             end
 
             if reflection.type.present?
               conditions[reflection.type] = self.class.name
             end
-            group_by = "#{table_alias_name}.#{aggregate_association.foreign_key}"
+            group_by = "#{table_reference}.#{aggregate_association.foreign_key}"
 
             preloaded_data = Hash[association_scope
               .where(conditions)
